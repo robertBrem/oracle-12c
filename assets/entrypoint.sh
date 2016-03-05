49a6eed4131a25d379ec2a87d098a9795fbe7871
robertBrem <brem_robert@hotmail.com>
2016-03-05 08:30:46 +0100

    move chmod und chown to entrypoint

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -8,5 +8,8 @@ if [ ! -d "/opt/oracle/app/product/12.1.0/dbhome_1" ]; then
 	/assets/install.sh
 fi
 
+chown -R oracle:oinstall /opt/oracle/dpdump
+chmod 777 /opt/oracle/dpdump
+
 su oracle -c "/assets/entrypoint_oracle.sh"
 
